c82c8be19ac4b429d86a8be3e291dac646d74ebe
NayabAkhtar12 <nayabakhtar955@gmail.com>
2024-04-23 13:06:23 +0500

    Prank Sound App Automation Started

---
+++ Pages/AirHorn1.cs
@@ -0,0 +1,181 @@
+﻿using AventStack.ExtentReports;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+
+namespace Live_Earth_Map.Pages
+{
+    class AirHorn
+    {
+        private AppiumDriver<IWebElement> driver;
+        private ExtentTest Test;
+        ExtentReports Extent = new ExtentReports();
+        //Constructor
+        public AirHorn(AppiumDriver<IWebElement> driver, ExtentTest test)
+        {
+            this.driver = driver;
+            this.Test = test;
+        }
+
+        public void NearByPlacesHome()
+        {
+            ExtentTest test = Extent.CreateTest("NearBy Places Report 1");
+            try
+            {
+                AirHornMenu.Click();
+                AirHorn1.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+            }
+            try
+            {
+                AirHorn2.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+            }
+            try
+            {
+                AirHorn3.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+            }
+            try
+            {
+                AirHorn4.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+            }
+            try
+            {
+                AirHorn5.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+            }
+            try
+            {
+                AirHorn6.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+
+            }
+
+            try
+            {
+                AirHorn7.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+
+            }
+
+            try
+            {
+                AirHorn8.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+
+            }
+
+            try
+            {
+                AirHorn9.Click();
+                PlayButton.Click();
+                VolumeUp.Click();
+                VolumeDown.Click();
+                Loop.Click();
+                BackButton1.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occurred while clicking Zoo: " + ex.Message);
+                Test.Log(Status.Fail, $"Test failed due to: {ex.Message}");
+
+            }
+        }
+
+        public IWebElement AirHornMenu => driver.FindElementByXPath("//android.widget.TextView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/name\" and @text=\"Air Horn\"]");
+
+        public IWebElement BackButton1 => driver.FindElementByAccessibilityId("Navigate up");
+        public IWebElement VolumeUp => driver.FindElementById("com.pranksound.fartsound.hornsound.haircut.soundprank:id/ivolplus");
+        public IWebElement VolumeDown => driver.FindElementById("com.pranksound.fartsound.hornsound.haircut.soundprank:id/ivvolminus");
+        public IWebElement Loop => driver.FindElementById("com.pranksound.fartsound.hornsound.haircut.soundprank:id/sCheck");
+        public IWebElement PlayButton => driver.FindElementById("com.pranksound.fartsound.hornsound.haircut.soundprank:id/ivPPCv");
+        public IWebElement AirHorn1 => driver.FindElementByXPath("//android.widget.TextView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/name\" and @text=\"Air Horn 1\"]");
+
+        public IWebElement AirHorn2 => driver.FindElementByXPath("(//android.view.ViewGroup[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/content_container\"])[2]");
+
+        public IWebElement AirHorn3 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[3]");
+
+        public IWebElement AirHorn4 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[4]");
+
+        public IWebElement AirHorn5 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[5]");
+
+        public IWebElement AirHorn6 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[6]");
+        public IWebElement AirHorn7 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[7]");
+        public IWebElement AirHorn8 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[8]");
+        public IWebElement AirHorn9 => driver.FindElementByXPath("(//android.widget.ImageView[@resource-id=\"com.pranksound.fartsound.hornsound.haircut.soundprank:id/icon\"])[9]");
+
+    }
+}
---
+++ Test_Class/Test_Class.cs
@@ -0,0 +1,38 @@
+﻿using AventStack.ExtentReports;
+using Live_Earth_Map.Core;
+using Live_Earth_Map.Pages;
+
+namespace Live_Earth_Map.Test_Class
+{
+    [TestClass]
+    public class Test_Class : TestInitiallize
+    {
+        AirHorn AirHorn;
+        [TestMethod]
+        public void AirHornSounds()
+        {
+            ExtentTest test = Extent.CreateTest("Famous Places Report");
+            AirHorn = new AirHorn(driver, test);
+            AirHorn.NearByPlacesHome();
+        }
+
+        AnimalSounds AnimalSound;
+        [TestMethod]
+        public void AnimalSounds()
+        {
+            ExtentTest test = Extent.CreateTest("Famous Places Report");
+            AnimalSound = new AnimalSounds(driver, test);
+            AnimalSound.AnimalSound();
+        }
+
+        FartSound FartSound;
+
+        [TestMethod]
+        public void FartSounds()
+        {
+            ExtentTest test = Extent.CreateTest("Famous Places Report");
+            FartSound = new FartSound(driver, test);
+            FartSound.FartSoundTest();
+        }
+    }
+}
